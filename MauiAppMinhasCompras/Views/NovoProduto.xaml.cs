aac936d0d6e235e5ea6e19794a4418591d51d2d3
Pedro-Dagama <pedrohigor082019@outlook.com>
2025-08-25 16:21:22 -0300

    Adiciona suporte a banco de dados e melhorias na UI
    Implementa a classe SQLiteDatabaseHelper em App.xaml.cs para gerenciar a conexão com o banco de dados SQLite. Atualiza a página ListaProduto.xaml com novo título e itens de toolbar. Adiciona navegação para NovoProduto e tratamento de exceções. Modifica NovoProduto.xaml para incluir campos de entrada e uma toolbar com opção de salvar. Atualiza NovoProduto.xaml.cs para inserir produtos no banco de dados e exibir alertas de sucesso ou erro.

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,33 @@
-namespace MauiAppMinhasCompras.Views;
+using Microsoft.Maui.Controls;
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
 
-public partial class NovoProduto : ContentPage
+namespace MauiAppMinhasCompras.Views
 {
-	public NovoProduto()
-	{
-		InitializeComponent();
-	}
+    public partial class NovoProduto : ContentPage
+    {
+        public NovoProduto()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clickedd(object sender, EventArgs e)
+        {
+            try {
+                Produto p = new Produto
+                {
+                    Descricao = txt_descricao.Text,
+                    Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                    Preco = Convert.ToDouble(txt_preco.Text)
+                };
+
+                await App.Db.Insert(p);
+                await DisplayAlert("sucesso", "registro inserido", "ok");
+
+            }catch(Exception ex)
+            {
+                await DisplayAlert("ops", ex.Message, "ok");
+            }
+        }
+    }
 }
